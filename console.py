12ae9fb035891f75a6c1e764150ae0474bbc0f69
blackoaz <pauloakello56@gmail.com>
2023-05-15 05:38:28 -0700

    adding console file

--- console.py
+++ console.py
@@ -1,19 +1,30 @@
+#!/usr/bin/python3
 import cmd
 
 
-class HelloWorld(cmd.Cmd):
+class HBNBCommand(cmd.Cmd):
     """Creating a terminal in python"""
+    prompt = "(hbnb)"
 
-    def do_greet(self, person):
-        """Function for grreting"""
-        if person:
-            print("Hello," + person)
-        else:
-            print("hi")
+    def do_EOF(self, args):
+        """Function for end of file"""
+        return True
+
+    def emptyline(self):
+        """Function for emptyline"""
+        pass
+
+    def help_EOF(self):
+        """Help function for EOF method"""
+        print("Function for exitting the program")
+
+    def help_quit(self):
+        """Help function for quit"""
+        print("Function for quiring the console")
 
     def do_quit(self, args):
         return True
 
 
 if __name__ == "__main__":
-    HelloWorld().cmdloop()
+    HBNBCommand().cmdloop()
