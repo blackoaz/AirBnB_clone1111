1a851a620c0b8da5b71a4fa882cd2872a6bc3b10
blackoaz <pauloakello56@gmail.com>
2023-05-15 02:35:20 -0700

    updates to base_model

--- models/base_model.py
+++ models/base_model.py
@@ -19,10 +19,12 @@ class BaseModel():
                     self.__dict__[k] = datetime.strptime(v, date_fmt)
                 else:
                     self.__dict__[k] = v
+        #else:
+           # models.storage.new(self)
 
     def save(self):
         self.updated_at = datetime.today()
-        models.storage.save()
+        #models.storage.save()
 
     def to_dict(self):
         """method for creating a dictionary for the class method"""
